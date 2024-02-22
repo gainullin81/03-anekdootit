2c6004f47b84de8f8fb860228501878174b7d5cc
alex Gainullin <againullin8@gmail.com>
2024-02-22 21:09:25 +0200

    1.13*: anekdootit step2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-//1.12*: anekdootit step1
+//1.13*: anekdootit step2 
 import React, { useState } from 'react';
 
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often.',
+    'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
@@ -11,22 +11,32 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
     'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0));
+
   const handleNextAnecdote = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length);
     setSelected(randomIndex);
   }
 
+  const handleVote = () => {
+    const copy = [...points];
+    copy[selected] += 1;
+    setPoints(copy);
+  }
+
   return (
     <div>
-    <div>
-      {anecdotes[selected]}
+      <div>
+        <p>{anecdotes[selected]}</p>
+        <p>Has {points[selected]} votes</p>
+      </div>
+      <button onClick={handleVote}>Vote</button>
+      <button onClick={handleNextAnecdote}>Next Anecdote</button>
     </div>
-    <button onClick={handleNextAnecdote}>Next Anecdote</button>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
